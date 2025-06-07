31462de42cdd65a2a3f487ffe46a79a98b636249
yashchowdhary34 <yashchowdhary34@gmail.com>
2025-06-07 20:45:59 +0530

    updated clerk middleware runtime config

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 // Add runtime configuration
-export const runtime = "edge";
+export const runtime = "experimental-edge";
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
