027c76b5243c331f202c534479211f9595473cff
yashchowdhary34 <yashchowdhary34@gmail.com>
2025-06-25 16:46:01 +0530

    Initial commit - setup prisma schema and env variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  { ignores: ["src/generated/prisma/**", "node_modules/**"] },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
